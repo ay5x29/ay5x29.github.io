e34b976cc144dfb56b21fbfc1f4543bc33bb3d88
ay5x29 <161468128+ay5x29@users.noreply.github.com>
2024-04-01 00:26:30 +0800

    11.js

--- 11.js
+++ 11.js
@@ -6,26 +6,25 @@ function fh(){
   function tj() {  
     var cores = 0;  
     var questions = document.getElementsByClassName("question");  
-    var answers;  
-    var correctAnswer;  
-    var selectedAnswer;  
-  
+      
     for (var i = 0; i < questions.length; i++) {  
-        answers = questions[i].getElementsByTagName("input");  
-        // 假设第一个问题的正确答案是 "周三或周日"，第二个问题的正确答案是 "砍第三个"  
-        // 根据实际情况，您可能需要从其他地方获取正确答案，比如一个数组或者通过 AJAX 请求  
-        correctAnswer = (i === 0) ? "周三或周日" : "砍第三个";  
-  
-        for (var j = 0; j < answers.length; j++) {  
-            if (answers[j].checked) {  
-                selectedAnswer = answers[j].nextElementSibling.textContent.trim(); // 获取相邻的 <li> 标签的文本内容  
-                if (selectedAnswer === correctAnswer) {  
-                    cores++; // 如果答案正确，增加分数  
-                    break; // 不需要继续检查其他选项，因为只有一个会被选中  
-                }  
-            }  
+      var inputs = questions[i].getElementsByTagName("input");  
+      var correctAnswer;  
+        
+      // 根据问题的索引获取正确答案  
+      correctAnswer = (i === 0) ? "周三或周日" : "砍第三个"; // 这里应该是实际答案  
+        
+      for (var j = 0; j < inputs.length; j++) {  
+        if (inputs[j].type === 'radio' && inputs[j].checked) {  
+          var selectedAnswer = inputs[j].value; // 直接使用单选按钮的value属性  
+            
+          if (selectedAnswer === correctAnswer) {  
+            cores++;  
+            break; // 找到正确答案后退出循环  
+          }  
         }  
+      }  
     }  
-  
+      
     alert("你的分数是 " + cores + " 分");  
-}
+  }
